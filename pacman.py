3b6e404268f4b0294db9daf48e24b92426a3e13d
Dmitriy <dima.boyko.bd@gmail.com>
2024-02-12 22:17:34 +0200

    walls

--- pacman.py
+++ pacman.py
@@ -10,6 +10,11 @@ pygame.init()
 # resolution, display
 WIDTH = 900
 HEIGHT = 950 
+
+num1 = ((HEIGHT - 50) // 32 )
+num2 = (WIDTH // 30)
+num3 = 15 # fat factor for the player
+
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
 # framerate
@@ -40,9 +45,9 @@ counter = 0
 
 flicker = False
 
+turns_allowed = [False, False, False, False]
+
 def draw_board():
-    num1 = ((HEIGHT - 50) // 32 )  # how tall will be
-    num2 = (WIDTH // 30) # how widht 
     for i in range(len(level)):
         for j in range(len(level[i])):
             if level[i][j] == 1:
@@ -74,6 +79,54 @@ def draw_player():
     if direction == 3: # DOWN
         screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
 
+def check_position(centerx, centery):
+    turns = [False, False, False, False]
+    
+    if centerx // 30 < 29:
+        if direction == 0: # RIGHT
+            if level[centery // num1][(centerx - num3) // num2] < 3:
+                turns[1] = True
+        if direction == 1: # LEFT
+            if level[centery // num1][(centerx + num3) // num2] < 3:
+                turns[0] = True
+        if direction == 2: # UP
+            if level[(centery + num3) // num1][(centerx - num3) // num2] < 3:
+                turns[3] = True
+        if direction == 3: # DOWN
+            if level[(centery - num3) // num1][(centerx - num3) // num2] < 3:
+                turns[2] = True
+
+        if direction == 2 or direction == 3:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num3) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num3) // num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centerx % num1 <= 18:
+                if level[centery // num1][(centerx - num2) // num2] < 3:
+                    turns[1] = True
+                if level[centery // num1][(centerx + num2) // num2] < 3:
+                    turns[0] = True
+                    
+        if direction == 0 or direction == 1:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num1) // num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num1) // num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centerx % num1 <= 18:
+                if level[centery // num1][(centerx - num3) // num2] < 3:
+                    turns[1] = True
+                if level[centery // num1][(centerx + num3) // num2] < 3:
+                    turns[0] = True          
+
+
+    else:
+        turns[0] = True
+        turns[1] = True
+
+    return turns
+
 
 # main game loop 
 
@@ -94,19 +147,32 @@ while run:
 
     draw_player()
 
+    center_x = player_x + 23
+    center_y = player_y + 24
+    turns_allowed = check_position(center_x, center_y)
+
     for event in pygame.event.get():
         # to leave loop by cross
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                direction = 0
+                direction_command = 0
             if event.key == pygame.K_LEFT:
-                direction = 1
+                direction_command = 1
             if event.key == pygame.K_UP:
-                direction = 2
+                direction_command = 2
             if event.key == pygame.K_DOWN:
-                direction = 3
+                direction_command = 3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT and direction_command == 0:
+                direction_command = direction
+            if event.key == pygame.K_LEFT and direction_command == 1:
+                direction_command = direction
+            if event.key == pygame.K_UP and direction_command == 2:
+                direction_command = direction
+            if event.key == pygame.K_DOWN and direction_command == 3:
+                direction_command = direction
 
     pygame.display.flip()
 pygame.quit()
